e4b305a3e2fea076df1173169ac99587fcbca54f
gabrielbmotta <gabrielbenmotta@gmail.com>
2022-03-15 16:55:41 -0400

    chnaging script to install (to be run once) and run (to be run when you want to run)

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
-:;# This script performs generates and copies the necesary library dependencies for running qt-projects both for 
-:;# dynamic and for staic builds. 
+:;# This script performs generates and copies the necesary library dependencies for running qt-projects both for
+:;# dynamic and for staic builds.
 :;#
 :;# This file is part of the MNE-CPP project. For more information visit: https://mne-cpp.github.io/
 :;#
 :;# This script is based on an open-source cross-platform script template.
 :;# For more information you can visit: https://github.com/juangpc/multiplatform_bash_cmd
-:;# 
+:;#
 
 :<<BATCH
     :;@echo off
     :; # ########## WINDOWS SECTION #########################
 
     echo Windows not yet supported.
-        
+
     :; # ########## WINDOWS SECTION ENDS ####################
     :; # ####################################################
     exit /b
 BATCH
 
-if [ "$(expr substr $(uname -s) 1 5)" == "Linux"  || "$(uname)" == "Darwin"]; then
-    
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux"  || "$(uname)" == "Darwin" ]; then
+
     # ######################################################
     # ############## LINUX SECTION #########################
 
@@ -28,7 +28,7 @@ if [ "$(expr substr $(uname -s) 1 5)" == "Linux"  || "$(uname)" == "Darwin"]; th
         then
         PYTHON_OK=true
         echo Python 3 version meets minimum requirements
-        else 
+        else
         PYTHON_OK=false
         echo Python 3 version does not meet minimum requirements
     fi
@@ -40,16 +40,6 @@ if [ "$(expr substr $(uname -s) 1 5)" == "Linux"  || "$(uname)" == "Darwin"]; th
         . venv/bin/activate
         pip install -r requirements-api.txt
         pip install fieldline_api-0.0.13-py3-none-any.whl
-
-        # ft buffer
-        ./buffer/linux/buffer &> /dev/null &
-        BUFFER_PID=$!
-        echo Fieldtrip Buffer running with pid ${BUFFER_PID}
-
-        # Fieldline
-        python3 mne_fieldline.py
-
-        kill -9 ${BUFFER_PID}
     fi
 
     # ############## LINUX SECTION ENDS ####################
