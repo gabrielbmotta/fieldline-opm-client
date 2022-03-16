295c9a49f41e337c2ded4b3480ad445dfea62ac0
gabrielbmotta <gabrielbenmotta@gmail.com>
2022-03-16 14:21:40 -0400

    Got rid of all of the old code, added new connecting and tuning functions based on example

--- mne_fieldline.py
+++ mne_fieldline.py
@@ -1,52 +1,74 @@
-import mne_fieldline_lib as lib
+from fieldline_api.fieldline_service import FieldLineService
 
-def connect():
-    print("About to Connect")
-    lib.init_fieldline_connection()
-    lib.init_fieldtrip_connection()
+import logging
+import argparse
+import queue
+import time
+import sys
+
+fieldline_client = None
+
+
+def call_done():
+    global done
+    done = True
+
+def connect(ip_list):
+    fieldline_client = FieldLineService(ip_list)
+    sensors = fieldline_client.load_sensors()
+    print(f"Sensors available: {sensors}")
 
 def tune_sensors():
-    lib.init_sensors()
+    global fieldline_client
+    if isinstance(fieldline_client, FieldLineService):
+        sensors = fieldline_client.load_sensors()
+        fieldline_client.set_closed_loop(True)
+
+        fieldline_client.restart_sensors(sensors, on_next=lambda c_id, s_id: print(f'sensor {c_id}:{s_id} finished restart'),
+                                on_error=lambda c_id, s_id, err: print(f'sensor {c_id}:{s_id} failed with {hex(err)}'),
+                                on_completed=lambda: call_done())
+        while not done:
+            time.sleep(1)
+
+        fieldline_client.coarse_zero_sensors(sensors,
+                                    on_next=lambda c_id, s_id: print(f'sensor {c_id}:{s_id} finished coarse zero'),
+                                    on_error=lambda c_id, s_id, err: print(
+                                        f'sensor {c_id}:{s_id} failed with {hex(err)}'),
+                                    on_completed=lambda: call_done())
+        while not done:
+            time.sleep(1)
+
+        fieldline_client.fine_zero_sensors(sensors, on_next=lambda c_id, s_id: print(f'sensor {c_id}:{s_id} finished fine zero'),
+                                  on_error=lambda c_id, s_id, err: print(
+                                      f'sensor {c_id}:{s_id} failed with {hex(err)}'), on_completed=lambda: call_done())
+        while not done:
+            time.sleep(1)
+
 
 def start_measurement():
-    lib.init_acquisition()
-    # if lib.are_sensors_ready():
-    #     lib.init_acquisition()
-    # else:
-    #     print("Sensors are not initialized")
+    pass
 
 def stop_measurement():
-    lib.stop_measurement()
+    pass
 
 def disconnect():
-    lib.stop_service()
+    pass
 
 def print_commands():
     print("Commands:")
-    print("\tInitialize sensors - init")
-    print("\tStart Measurment - start")
-    print("\tStop Measurement - stop")
-    print("\tDisconnect and exit - exit")
+    print("connect \t Connects to fieldline chassis and fieldtrip buffer")
+    print("init \t\t Restart and tune sensors.")
+    print("start \t\t Start receiving data.")
+    print("stop \t\t Stop receiving data.")
+    print("exit \t\t Stop receiving data and quit.")
+    print("help \t\t Prints command list.")
 
 if __name__ == "__main__":
-    connect()
-
-    print("\t ************************************************")
-    print("\t ************************************************")
-    print("\t ******                                   *******")
-    print("\t ******             IMPORTANT             *******")
-    print("\t ******                                   *******")
-    print("\t ******     MAKE SURE YOU INITIALIZE      *******")
-    print("\t ******   THE SENSORS BEFORE YOU START    *******")
-    print("\t ******                                   *******")
-    print("\t ************************************************")
-    print("\t ************************************************")
-
+    print("MNE Fieldline Connector v0.0.2\n")
+    print_commands()
+    print("")
     continue_loop = True
     while(continue_loop):
-
-        print_commands()
-
         command = input("Select command: ")
         if command == "start":
             print("Starting measurement...")
@@ -56,10 +78,11 @@ if __name__ == "__main__":
             stop_measurement()
         elif command == "init":
             tune_sensors()
-        elif command == "exit":
-            print("Exiting program.")
+        elif command == "exit" or command == "quit":
             continue_loop = False
-            print("Destructor called")
-
+        elif command == "connect":
+            connect()
+        elif command == "help":
+            print_commands()
     stop_measurement()
     disconnect()
